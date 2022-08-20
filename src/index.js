a44d1702db5ce1b07123feda2950938d35dee651
ivan52945 <ivan52945@gmail.com>
2022-08-20 18:22:20 +0300

    feat: funish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	function ecran(symbol) {
+		return (("[ ] \ ^ $ . | ? * + ( )").includes(symbol)) ? `\\${symbol}` : `${symbol}`;
+	}
+	const bracketExpGroup = [];
+	bracketsConfig.forEach((element) => {
+		bracketExpGroup.push(new RegExp(`${ecran(element[0])}${ecran(element[1])}`, "g"));
+	});
+	while (true) {
+		let length = str.length;
+		for (let exp of bracketExpGroup) {
+			str = str.replace(exp, "");
+		}
+		if (str.length == length) { break; }
+	}
+	return (str.length > 0) ? false : true;
 }
